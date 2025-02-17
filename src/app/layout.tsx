5af247e94c64a809b0a7f1a913ead23cf7895bf2
anasahmedshaikh <anasahmedshaikh.work@gmail.com>
2025-02-17 10:36:32 +0500

    enabled analytics and peed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode;}>) {
   
   return (
     <html lang="en">
+      <Analytics/>
+      <SpeedInsights/>
       <body>
         {children}
       </body>
